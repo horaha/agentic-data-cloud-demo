3ccbc69c54e7dc2d65a87078173225114f4745d6
Junho Jang <junhojang@google.com>
2026-07-02 22:29:24 +0900

    refactor: rename VPC subnets from asne3 to usce1 to match default region

--- terraform/infra/vpc.tf
+++ terraform/infra/vpc.tf
@@ -4,9 +4,9 @@ module "vpc" {
 
   project_id            = var.project_id
   network_name          = "adc-demo-vpc"
-  public_subnet_name    = "adc-demo-public-subnet-asne3"
+  public_subnet_name    = "adc-demo-public-subnet-usce1"
   public_ip_cidr_range  = "10.0.0.0/24"
-  private_subnet_name   = "adc-demo-private-subnet-asne3"
+  private_subnet_name   = "adc-demo-private-subnet-usce1"
   private_ip_cidr_range = "10.0.100.0/24"
   region                = var.region
 
