d89d96c391e73f1a73cf46649a21e8a905a0fab3
Junho Jang <junhojang@google.com>
2026-07-02 22:45:38 +0900

    fix: work around Qwiklabs 403 IAM permission errors in bigquery.tf

--- terraform/infra/bigquery.tf
+++ terraform/infra/bigquery.tf
@@ -10,14 +10,15 @@ resource "time_sleep" "wait_for_service_agents" {
   create_duration = "10s"
 }
 
-# BigQuery Data Transfer service agent 권한 부여
-resource "google_project_iam_member" "bqp_dts_permissions" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountTokenCreator"
-  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-bigquerydatatransfer.iam.gserviceaccount.com"
-
-  depends_on = [time_sleep.wait_for_service_agents]
-}
+# BigQuery Data Transfer service agent 권한 부여 (Qwiklabs 환경 우회를 위해 주석 처리)
+# US 멀티 리전 내 복제(Colocated Copy)의 경우 토큰 생성자 권한이 요구되지 않으므로 제거 가능합니다.
+# resource "google_project_iam_member" "bqp_dts_permissions" {
+#   project = var.project_id
+#   role    = "roles/iam.serviceAccountTokenCreator"
+#   member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-bigquerydatatransfer.iam.gserviceaccount.com"
+# 
+#   depends_on = [time_sleep.wait_for_service_agents]
+# }
 
 
 # 대상 데이터셋 생성
@@ -25,7 +26,7 @@ resource "google_bigquery_dataset" "thelook" {
   dataset_id                  = "thelook_ecommerce"
   friendly_name               = "TheLook eCommerce"
   description                 = "Cloned public dataset thelook_ecommerce"
-  location                    = var.region
+  location                    = "US" # "US" 멀티 리전으로 설정하여 cross-region 복제 요건을 제거
   default_table_expiration_ms = null
 
   depends_on = [module.apis]
@@ -33,10 +34,11 @@ resource "google_bigquery_dataset" "thelook" {
 
 # 데이터 복제 구성
 resource "google_bigquery_data_transfer_config" "thelook_copy" {
-  depends_on = [google_project_iam_member.bqp_dts_permissions, google_bigquery_dataset.thelook]
+  # bqp_dts_permissions 의존성 제거
+  depends_on = [google_bigquery_dataset.thelook]
 
   display_name           = "Replicate thelook_ecommerce"
-  location               = var.region
+  location               = "US" # 데이터셋 위치와 동일하게 US 설정
   data_source_id         = "cross_region_copy"
   schedule               = "every 24 hours"
   destination_dataset_id = google_bigquery_dataset.thelook.dataset_id
@@ -48,11 +50,14 @@ resource "google_bigquery_data_transfer_config" "thelook_copy" {
   }
 }
 
-# Dataplex Service Agent에 빅쿼리 데이터 편집자(BigQuery Data Editor) 권한 부여 (자동 테이블 생성을 위해 필요)
-resource "google_project_iam_member" "dataplex_bigquery_editor" {
-  project = var.project_id
-  role    = "roles/bigquery.dataEditor"
-  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-dataplex.iam.gserviceaccount.com"
+# Dataplex Service Agent에 빅쿼리 데이터 편집자(BigQuery Data Editor) 권한 부여
+# Qwiklabs의 프로젝트 수준 IAM 제한을 피하기 위해 데이터셋 수준 권한(google_bigquery_dataset_iam_member)으로 변경합니다.
+# 만약 이 설정도 Qwiklabs 조직 정책 도메인 제한으로 인해 실패할 경우, 이 리소스 전체를 주석 처리해 주세요.
+resource "google_bigquery_dataset_iam_member" "dataplex_bigquery_editor" {
+  dataset_id = google_bigquery_dataset.thelook.dataset_id
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-dataplex.iam.gserviceaccount.com"
 
   depends_on = [time_sleep.wait_for_service_agents]
 }
+
