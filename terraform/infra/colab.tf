78ce8f141d56bd9998724acce533631457abfa94
Junho Jang <junhojang@google.com>
2026-07-13 22:29:21 +0900

    fix: include machine type in Colab resource names to prevent deletion race condition

--- terraform/infra/colab.tf
+++ terraform/infra/colab.tf
@@ -6,7 +6,7 @@ resource "random_id" "colab_suffix" {
 
 # Colab Enterprise 런타임 템플릿 생성
 resource "google_colab_runtime_template" "colab_template_usce1" {
-  name         = "adc-demo-template-${random_id.colab_suffix.hex}"
+  name         = "adc-demo-template-${var.colab_machine_type}-${random_id.colab_suffix.hex}"
   display_name = "Colab Runtime Template (us-central1)"
   location     = var.region
   description  = "Colab Enterprise Runtime Template in us-central1"
@@ -30,7 +30,7 @@ resource "google_colab_runtime_template" "colab_template_usce1" {
 
 # 템플릿 기반으로 실습용 런타임(VM) 사전 생성
 resource "google_colab_runtime" "colab_runtime" {
-  name         = "adc-demo-runtime-${random_id.colab_suffix.hex}"
+  name         = "adc-demo-runtime-${var.colab_machine_type}-${random_id.colab_suffix.hex}"
   display_name = "ADC Demo Runtime"
   location     = var.region
   
@@ -40,6 +40,7 @@ resource "google_colab_runtime" "colab_runtime" {
   notebook_runtime_template_ref {
     notebook_runtime_template = google_colab_runtime_template.colab_template_usce1.id
   }
+
 
   depends_on = [google_colab_runtime_template.colab_template_usce1]
 }
